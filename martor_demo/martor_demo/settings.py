561e370c22a104cec10da652ecd2d50d94f6319e
gradedproject <gradedproject1@gmail.com>
2021-04-28 11:14:53 +0000

    make coorection in admin

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-import tempfile
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,5 +141,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static"),
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
